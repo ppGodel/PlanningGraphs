f694db1976d5713f1d262b7c14ccdd1df16f19fe
SatuElisa <elisa.schaeffer@gmail.com>
2019-12-06 14:27:56 -0600

    Separate individual graphs into files of their own

---
+++ extraction/separate.sh
@@ -0,0 +1,11 @@
+while read p; do
+    g=`echo "$p"`
+    if test -f "$g.graph"; then
+        echo $g 'has already been extracted'
+    else
+        echo 'Extracting' $g
+        awk -v graph=$g '{if ($1 == graph) { for (i = 3; i <= NF; i++) { printf $i" " }; printf "\n" } }' < nodes2.csv > $g.graph
+        awk -v graph=$g '{if ($1 == graph) { for (i = 2; i <= NF; i++) { printf $i" " }; printf "\n" } }' < edges2.csv >> $g.graph
+        echo $g 'extracted'
+    fi
+done <graphs2.csv
